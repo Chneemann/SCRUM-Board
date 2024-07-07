aaafd972d3b82abb422689e4952a1b032b007e46
Chneemann <Andre-Ke@gmx.de>
2024-07-07 10:36:17 +0200

    display task created date

--- frontend/src/app/component/board/view-task/view-task.component.ts
+++ frontend/src/app/component/board/view-task/view-task.component.ts
@@ -20,6 +20,8 @@ export class ViewTaskComponent {
 
   openDescription: boolean = false;
   openDescriptionTaskId: string = '';
+  openDate: boolean = false;
+  openDateTaskId: string = '';
 
   constructor(
     private taskColorService: TaskColorsService,
@@ -32,13 +34,22 @@ export class ViewTaskComponent {
 
   startDragging(status: string) {
     this.startDraggingStatus.emit(status);
-    this.openDescription = !this.openDescription;
+    this.openDescription = false;
+    this.openDate = false;
   }
 
   toggleDescription(taskIndex: string, event: MouseEvent) {
+    console.log(taskIndex, event);
+
     event.stopPropagation();
     this.openDescription = !this.openDescription;
     this.openDescriptionTaskId = taskIndex;
+  }
+
+  toggleDate(taskIndex: string, event: MouseEvent) {
+    event.stopPropagation();
+    this.openDate = !this.openDate;
+    this.openDateTaskId = taskIndex;
   }
 
   firstLetter(word: string) {
@@ -47,5 +58,24 @@ export class ViewTaskComponent {
 
   findColor(color: string) {
     return this.taskColorService.findColor(color);
+  }
+
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
+    let hours = date.getHours();
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // die Stunde '0' sollte '12' sein
+    const strHours = String(hours).padStart(2, '0');
+
+    return `${day}.${month}.${year} ${strHours}:${minutes}:${seconds} ${ampm}`;
   }
 }
