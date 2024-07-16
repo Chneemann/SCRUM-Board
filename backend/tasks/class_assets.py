c150cd1f4758e9d35e6c031c2687903cb9e35835
Chneemann <Andre-Ke@gmx.de>
2024-07-16 09:11:35 +0200

    clean code => added class_assets

---
+++ backend/tasks/class_assets.py
@@ -0,0 +1,23 @@
+PRIORITY_CHOICES = [
+  ('low', 'Low'),
+  ('medium', 'Medium'),
+  ('hight', 'High'),
+]
+
+STATUS_CHOICES = [
+    ('todo', 'To-do'), 
+    ('dotoday', 'Do today'), 
+    ('inprogress', 'In progress'), 
+    ('done', 'Done')
+]
+
+COLOR_CHOICES = [
+  ('yellow', 'Yellow'), 
+  ('green', 'Green'), 
+  ('blue', 'Blue'), 
+  ('red', 'Red'), 
+  ('orange', 'Orange'), 
+  ('purple', 'Purple'), 
+  ('magenta', 'Magenta'), 
+  ('cyan', 'Cyan'),
+]
--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from .class_assets import PRIORITY_CHOICES, STATUS_CHOICES, COLOR_CHOICES
 
 class SubtaskItem(models.Model):
     title = models.CharField(max_length=50,blank=True)
@@ -17,9 +18,9 @@ class TaskItem(models.Model):
     due_date = models.DateField(null=True, blank=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,blank=True)
     assigned = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='assigned_tasks', blank=True)
-    priority = models.CharField(max_length=10, choices=[('low', 'Low'), ('medium', 'Medium'), ('high', 'High')], default='medium') 
-    status = models.CharField(max_length=10, choices=[('todo', 'To-do'), ('dotoday', 'Do today'), ('inprogress', 'In progress'), ('done', 'Done')], default='todo')
-    color = models.CharField(max_length=10, choices=[('yellow', 'Yellow'), ('green', 'Green'), ('blue', 'Blue'), ('red', 'Red'), ('orange', 'Orange'), ('purple', 'Purple'), ('magenta', 'Magenta'), ('cyan', 'Cyan')], default='todo')
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='medium') 
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='todo')
+    color = models.CharField(max_length=10, choices=COLOR_CHOICES, default='todo')
     
     def __str__(self):
         return f'({self.id}) {self.title} - {self.description}'
