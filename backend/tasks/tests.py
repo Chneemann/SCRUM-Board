873ec7909d87872bd72db9ecdfb7526b65bed6f3
Chneemann <Andre-Ke@gmx.de>
2024-07-30 11:27:35 +0200

    added task test

--- backend/tasks/tests.py
+++ backend/tasks/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TaskTest(TestCase):
+    def test_task_page(self):
+        response = self.client.get("/tasks/")
+        self.assertEqual(response.status_code, 200)
